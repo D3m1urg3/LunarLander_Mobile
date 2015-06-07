77157f5be3edf765edbd73a1ddd9502638720200
TheSharkRider <yonatan.calderon@gmail.com>
2015-06-07 14:01:16 +0200

    Added percent numbers to Gas UI
    + Added number prefab for UI uses.
    + Added percent to GasUI
    
    GAS UI FULLY OPERATIONAL!

--- Assets/Scripts/Levels/UI_Fuel_Manager.cs
+++ Assets/Scripts/Levels/UI_Fuel_Manager.cs
@@ -7,12 +7,22 @@ public class UI_Fuel_Manager : MonoBehaviour {
 	public SpriteRenderer [] fuel_level_sprites = new SpriteRenderer[18];
 	public SpriteRenderer fuel_warning_sprite;
 
+	public UI_Number number_0x;
+	public UI_Number number_x0;
+
+	public int fuel_reserve_level;
+
 	int max_fuel;
 	int fuel;
 
 	int prev_fuel_level;
 	int fuel_level;
 	float fuel_per_slice;
+
+	int fuel_percent;
+	int percent_0x;
+	int percent_x0;
+
 
 	void Awake()
 	{
@@ -28,16 +38,28 @@ public class UI_Fuel_Manager : MonoBehaviour {
 		max_fuel = man.shipManager.fuel;
 		fuel_per_slice = (float)max_fuel / 18;
 		prev_fuel_level = (int) ((float) fuel / fuel_per_slice);
+		fuel_percent = (int)(fuel / max_fuel) * 100;
+		number_0x.bluered = number_x0.bluered = false; //Numbers in blue 
+		number_0x.number = number_x0.number = 9;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		// Fuel Corona
+
 		fuel = man.shipManager.fuel;
 
 		fuel_level = (int) ((float) fuel / fuel_per_slice);
 
-		Debug.Log ("Fuel Level: " + fuel_level);
+		//Debug.Log ("Fuel Level: " + fuel_level);
+
+		if(fuel_level <= fuel_reserve_level)
+		{
+			fuel_warning_sprite.enabled = true; //activate warning ring
+			number_0x.bluered = number_x0.bluered = true; //Numbers in red 
+		}
+
 
 		if( fuel_level != prev_fuel_level && fuel_level < 17)
 		{
@@ -45,6 +67,25 @@ public class UI_Fuel_Manager : MonoBehaviour {
 			if(fuel_level == 0)
 				fuel_level_sprites[0].enabled = false;
 		}
+
+		fuel_percent = (int)(100.0f * (float)fuel / max_fuel) ;
+
+		//Fuel percent
+		if(fuel_percent != 100)
+		{
+			percent_x0 = fuel_percent / 10;
+			percent_0x = fuel_percent % 10;
+
+			number_0x.number = percent_0x;
+			number_x0.number = percent_x0;
+		}
+		else
+		{
+			percent_0x = percent_x0 = 9;
+		}
+
+		Debug.Log("fuel percent= " + fuel_percent);
+		Debug.Log("percent digits= " + number_x0.number + " " + number_0x.number);
 
 	}
 
---
+++ Assets/Scripts/Levels/UI_Number.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Number : MonoBehaviour {
+
+	public int number;
+
+	public SpriteRenderer [] number_renders_blue = new SpriteRenderer[10];
+	public SpriteRenderer [] number_renders_red = new SpriteRenderer[10];
+
+	public bool bluered; // 0 blue - 1 red
+
+	void Awake()
+	{
+		number = 0;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		foreach (SpriteRenderer sprite in number_renders_blue)
+			sprite.enabled = false;
+
+		foreach (SpriteRenderer sprite in number_renders_red)
+			sprite.enabled = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		Debug.Log ("num: " + number); 
+
+		if (number <= 9) 
+		{
+			switch(bluered)
+			{
+			case true: // red
+
+				foreach (SpriteRenderer sprite in number_renders_red)
+					sprite.enabled = false;
+
+				number_renders_red[number].enabled = true;
+		
+				break;
+
+			case false: // blue
+
+				foreach (SpriteRenderer sprite in number_renders_blue)
+					sprite.enabled = false;
+
+				number_renders_blue[number].enabled = true;
+
+				break;
+			}
+
+		}
+		else if( number > 9)
+		{
+			Debug.LogError(gameObject.name + " class UI_Number: number > 9 out of range!");
+			return;
+		}
+
+
+	}
+
+}
