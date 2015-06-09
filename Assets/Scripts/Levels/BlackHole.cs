85411eef6f2317f685795da89b4e025047edd7ec
TheSharkRider <yonatan.calderon@gmail.com>
2015-06-09 17:54:11 +0200

    Update - see description
    + Updated sprites to lastest version
    + Added black hole animations
    + Eliminated spaceship engine thrust when idle
    + Fix spaceship thrust anim

--- Assets/Scripts/Levels/BlackHole.cs
+++ Assets/Scripts/Levels/BlackHole.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class BlackHole : MonoBehaviour {
 
-	public GameObject ship;
+	GameObject ship;
 	Rigidbody2D ship_physics;
 
+	Animator blackhole_anim;
+	AnimatorClipInfo clip;
 
 	public float gravity_push;
+
+	public bool isActive;
+	bool prev_state;
 
 	Vector2 gravity_force;
 
 	bool isInRange;
-
+	
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if (col.gameObject.name == ship.name)
@@ -24,16 +29,38 @@ public class BlackHole : MonoBehaviour {
 		if (col.gameObject.name == ship.name)
 			isInRange = false;
 	}
+
+	void Awake()
+	{
+		isActive = false;
+		prev_state = isActive;
+	}
 	// Use this for initialization
 	void Start () 
 	{
 		isInRange = false;
+		ship = man.shipManager.ship;
+		blackhole_anim = gameObject.GetComponent<Animator> ();
+
 		ship_physics = ship.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		//Blackhole animation
+		blackhole_anim.SetBool ("BlackHole_active", isActive);
+
+		if (prev_state != isActive) 
+		{
+
+			prev_state = isActive;
+		}
+
+
+
+
+		//Black Hole Behaviour
 		if(isInRange)
 		{
 			float ship_to_hole = (gameObject.transform.position - ship.transform.position).magnitude;
