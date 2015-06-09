47275dc337d5b8dfb3b43f6535cedce49bdd20a6
NuXareon <millas@gmail.com>
2015-06-09 18:35:41 +0200

    Selection menu mockup

---
+++ Assets/Scripts/SelectionMenu/SelectionManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[Serializable]
+public struct initialMission {
+	public int idx;
+	public int fuel;
+}
+
+public class SelectionManager : MonoBehaviour {
+
+	public initialMission[] fuel;
+	public Transform[] missions;
+
+	private Hashtable fuelHash;
+
+	// Checks which leves are avaiable to the player
+	private void loadSelectionMenu() {
+		for (int i = 0; i < missions.Length; ++i) {
+			if (PlayerPrefs.GetInt("level_" + i + "_score",-1) == -1) {
+				missions[i].Find("Disabled").gameObject.SetActive(true);
+				missions[i].Find("Enabled").gameObject.SetActive(false);
+			} else {
+				missions[i].Find("Enabled").gameObject.SetActive(true);
+				missions[i].Find("Disabled").gameObject.SetActive(false);
+				if (PlayerPrefs.GetInt("level_" + i + "_score",0) > 0) {
+					missions[i].Find("Enabled/Completed").gameObject.SetActive(true);
+				} else {
+					missions[i].Find("Enabled/Completed").gameObject.SetActive(false);
+				}
+			}
+		}
+	}
+
+	// Modify to get the correct values (or move it to a suitable location)
+	public void testButton(int lvl) {
+		PlayerPrefs.SetInt ("level_" + lvl + "_score", 100);
+		if ((lvl + 1) < missions.Length) {
+			if (PlayerPrefs.GetInt("level_" + (lvl+1) + "_score",-1) == -1) PlayerPrefs.SetInt("level_" + (lvl+1) + "_score", 0);
+			if (PlayerPrefs.GetInt ("level_" + (lvl+1) + "_fuel",-1) < 100) PlayerPrefs.SetInt ("level_" + (lvl+1) + "_fuel", 100);
+		}
+		//loadSelectionMenu ();
+		//Application.LoadLevel (lvl);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		// Temporary, for development porpuses
+		PlayerPrefs.DeleteAll ();
+		// Initialize score
+		// TODO: initialize fuel on especific levels (first levels of each zone)
+		for (int i = 0; i < missions.Length; ++i) {
+			if (!PlayerPrefs.HasKey("level_"+i+"_score")) PlayerPrefs.SetInt("level_"+i+"_score",-1);
+			if (!PlayerPrefs.HasKey("level_"+i+"_fuel")) PlayerPrefs.SetInt("level_"+i+"_fuel",-1);
+		}
+		// Make first mission avaiable allways (TODO: more than one initial mission avaiable?)
+		if (PlayerPrefs.GetInt ("level_0_score") == -1) {
+			PlayerPrefs.SetInt ("level_0_score",0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		loadSelectionMenu ();
+		//Debug.Log(PlayerPrefs.GetInt("level_1_score"));
+		//Debug.Log(PlayerPrefs.GetInt("level_2_fuel"));
+	}
+}
