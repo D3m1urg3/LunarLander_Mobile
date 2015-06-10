e7cbfd6c5bfc7a468d244ef7f0779a7cbfb2e0c4
TheSharkRider <yonatan.calderon@gmail.com>
2015-06-10 18:48:51 +0200

    just to sync

---
+++ Assets/Scripts/Levels/Shooting_Star.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting_Star : MonoBehaviour {
+
+	public float lifetime;
+	public bool isFading;
+	Animator star_anim;
+
+	void Awake()
+	{
+		star_anim = gameObject.GetComponent<Animator> ();
+	}
+
+	void OnEnable ()
+	{
+
+		Invoke ("FadeOut", lifetime);
+	}
+
+	void FadseOut()
+	{
+		gameObject.SetActive (false);
+	}
+
+	void OnDisable()
+	{
+		CancelInvoke ();
+	}
+
+}
