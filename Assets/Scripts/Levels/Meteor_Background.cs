574c34226c9e8fa09a6a99d39000d360c8f8858a
TheSharkRider <yonatan.calderon@gmail.com>
2015-06-10 20:19:19 +0200

    update

--- Assets/Scripts/Levels/Meteor_Background.cs
+++ Assets/Scripts/Levels/Meteor_Background.cs
@@ -4,20 +4,44 @@ using System.Collections.Generic;
 
 public class Meteor_Background : MonoBehaviour {
 
-	public bool isFading;
+	public float emission_time;
+	public int stars_on_screen = 4;
+	float deltaTime;
 
-	public GameObject shootingStar;
 
-	Animator star_anim;
+	public int pool_size = 10;
+	public GameObject shooting_star;
+	public List<GameObject> stars;
+
 
 
 	// Use this for initialization
 	void Start () {
-		isFading = false;
 
-		star_anim = shootingStar.GetComponent<Animator> ();
+		stars = new List<GameObject> ();
+
+		for(int i=0; i<pool_size; ++i)
+		{
+			GameObject obj = (GameObject)Instantiate(shooting_star);
+			obj.SetActive(false);
+			stars.Add (obj);
+		}
+
+		InvokeRepeating ("Starfall", emission_time, emission_time);
 	}
-	
+
+	void Starfall()
+	{
+		for(int i=0; i<stars.Count; ++i)
+		{
+			if(!stars[i].activeInHierarchy)
+			{
+
+				
+			}
+		}
+	}
+
 	// Update is called once per frame
 	void Update () 
 	{
--- Assets/Scripts/Levels/Shooting_Star.cs
+++ Assets/Scripts/Levels/Shooting_Star.cs
@@ -3,22 +3,35 @@ using System.Collections;
 
 public class Shooting_Star : MonoBehaviour {
 
+	public float speed;
 	public float lifetime;
 	public bool isFading;
 	Animator star_anim;
 
-	void Awake()
+
+	void Start()
 	{
 		star_anim = gameObject.GetComponent<Animator> ();
 	}
 
+   	void Update()
+	{
+		transform.Translate(-speed*Time.deltaTime*Vector3.right);	
+	}
+
 	void OnEnable ()
 	{
-
+		isFading = false;
 		Invoke ("FadeOut", lifetime);
 	}
 
-	void FadseOut()
+	void FadeOut()
+	{
+		isFading = true;
+		star_anim.SetBool ("isFading", isFading);
+	}
+
+	void Disable()
 	{
 		gameObject.SetActive (false);
 	}
