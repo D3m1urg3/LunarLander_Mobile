2a59c9a3476495f86ed18bd4097498bad21e12de
TheSharkRider <yonatan.calderon@gmail.com>
2015-06-10 16:23:55 +0200

    update just for sync

---
+++ Assets/Scripts/Levels/Meteor_Background.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Meteor_Background : MonoBehaviour {
+
+	public bool isFading;
+
+	public GameObject shootingStar;
+
+	Animator star_anim;
+
+
+	// Use this for initialization
+	void Start () {
+		isFading = false;
+
+		star_anim = shootingStar.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+
+	}
+}
