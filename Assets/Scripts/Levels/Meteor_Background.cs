24a3f3f8a8d16b7c6b7f087f26acc3953d25456d
TheSharkRider <yonatan.calderon@gmail.com>
2015-06-11 02:04:14 +0200

    Fix meteor storm
    now there are 3 types

--- Assets/Scripts/Levels/Meteor_Background.cs
+++ Assets/Scripts/Levels/Meteor_Background.cs
@@ -8,8 +8,14 @@ public class Meteor_Background : MonoBehaviour {
 	public float stars_lifetime;
 	public int stars_on_screen = 4;
 	public int pool_size = 10;
-	public GameObject shooting_star;
-	public List<GameObject> stars;
+	public GameObject shooting_star_close;
+	public GameObject shooting_star_mid;
+	public GameObject shooting_star_far;	
+	public List<GameObject> stars_close;
+	public List<GameObject> stars_mid;
+	public List<GameObject> stars_far;
+
+
 
 	int active_stars;
 
@@ -18,14 +24,29 @@ public class Meteor_Background : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		stars = new List<GameObject> ();
-
+		stars_close = new List<GameObject> ();
+		stars_mid = new List<GameObject> ();
+		stars_far = new List<GameObject> ();
+		
 		for(int i=0; i<pool_size; ++i)
 		{
-			GameObject obj = (GameObject)Instantiate(shooting_star);
+			GameObject obj;
+			//pool close star
+			obj = (GameObject)Instantiate(shooting_star_close);
 			obj.transform.parent = gameObject.transform;
 			obj.SetActive(false);
-			stars.Add (obj);
+			stars_close.Add (obj);
+			//pool mid star
+			obj = (GameObject)Instantiate(shooting_star_mid);
+			obj.transform.parent = gameObject.transform;
+			obj.SetActive(false);
+			stars_mid.Add (obj);
+			//pool far star
+			obj = (GameObject)Instantiate(shooting_star_far);
+			obj.transform.parent = gameObject.transform;
+			obj.SetActive(false);
+			stars_close.Add (obj);
+
 		}
 
 		InvokeRepeating ("Starfall", emission_time, emission_time);
@@ -34,27 +55,70 @@ public class Meteor_Background : MonoBehaviour {
 	void Starfall()
 	{
 		active_stars = 0;
-		for(int i=0; i<stars.Count; ++i)
-		{
-			if(!stars[i].activeInHierarchy)
-			{
-				active_stars++;
-				stars[i].transform.position = new Vector3(Random.Range(-7.0f,7.0f),gameObject.transform.position.y + Random.Range(-3.0f,3.0f),0.0f);
-				stars[i].transform.eulerAngles = new Vector3(0.0f,0.0f,45.0f);
-				Shooting_Star star_control = stars[i].GetComponent<Shooting_Star>();
-				star_control.lifetime = stars_lifetime + (float)Random.Range(0.0f,5.0f);
 
-				stars[i].SetActive(true);
-			}
-			if( active_stars > stars_on_screen)
+		for(int i=0; i<pool_size; ++i)
+		{
+
+			int select = Random.Range (0, 2);
+			
+			switch(select)
+			{
+			case 0:
+
+				if(!stars_close[i].activeInHierarchy)
+				{
+					active_stars++;
+					stars_close[i].transform.position = new Vector3(Random.Range(-7.0f,7.0f),gameObject.transform.position.y + Random.Range(-3.0f,3.0f),0.0f);
+					stars_close[i].transform.eulerAngles = new Vector3(0.0f,0.0f,45.0f);
+					Shooting_Star star_control = stars_close[i].GetComponent<Shooting_Star>();
+					star_control.lifetime = stars_lifetime + (float)Random.Range(0.0f,5.0f);
+					
+					stars_close[i].SetActive(true);
+				}
+				if( active_stars > stars_on_screen)
+					break;
+
 				break;
+			case 1:
+
+				if(!stars_mid[i].activeInHierarchy)
+				{
+					active_stars++;
+					stars_mid[i].transform.position = new Vector3(Random.Range(-7.0f,7.0f),gameObject.transform.position.y + Random.Range(-3.0f,3.0f),0.0f);
+					stars_mid[i].transform.eulerAngles = new Vector3(0.0f,0.0f,45.0f);
+					Shooting_Star star_control = stars_mid[i].GetComponent<Shooting_Star>();
+					star_control.lifetime = stars_lifetime + (float)Random.Range(0.0f,5.0f);
+					
+					stars_mid[i].SetActive(true);
+				}
+				if( active_stars > stars_on_screen)
+					break;
+
+				break;
+			case 2:
+				if(!stars_far[i].activeInHierarchy)
+				{
+					active_stars++;
+					stars_far[i].transform.position = new Vector3(Random.Range(-7.0f,7.0f),gameObject.transform.position.y + Random.Range(-3.0f,3.0f),0.0f);
+					stars_far[i].transform.eulerAngles = new Vector3(0.0f,0.0f,45.0f);
+					Shooting_Star star_control = stars_far[i].GetComponent<Shooting_Star>();
+					star_control.lifetime = stars_lifetime + (float)Random.Range(0.0f,5.0f);
+					
+					stars_far[i].SetActive(true);
+				}
+				if( active_stars > stars_on_screen)
+					break;
+
+				break;
+			}
+
 		}
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-	
+
 
 	}
 }
--- Assets/Scripts/Levels/Shooting_Star.cs
+++ Assets/Scripts/Levels/Shooting_Star.cs
@@ -5,39 +5,61 @@ public class Shooting_Star : MonoBehaviour {
 
 	public float speed;
 	public float lifetime;
-	public bool isFading;
-	Animator star_anim;
+	public float fading_time;
 
+	SpriteRenderer render;
+	float time_increment;
+	float timer;
+	float alpha;
+	public bool isFading;
 
 	void Start()
 	{
-		star_anim = gameObject.GetComponent<Animator> ();
+		render = gameObject.GetComponent<SpriteRenderer> ();
+		time_increment = fading_time / 30.0f;
+		timer = 0.0f;
+		isFading = false;
 	}
 
    	void Update()
 	{
+
 		transform.Translate(-speed*Time.deltaTime*Vector3.right);	
+
+
+		if (isFading) 
+		{
+			if (timer < time_increment)
+				timer += Time.deltaTime;
+			else if (render.color.a != 0f && render.color.a > 0f) 
+			{
+				float current_alpha = render.color.a;
+				render.color = new Color (render.color.r, render.color.g, render.color.b, render.color.a - 0.03333f);
+				timer = 0.0f;
+			} 
+			else if (render.color.a <= 0.0f) 
+			{				
+				render.color = new Color(render.color.r,render.color.g, render.color.b, 1.0f);
+				gameObject.SetActive (false);
+			}
+		}
+
 	}
 
 	void OnEnable ()
 	{
-		isFading = false;
 		Invoke ("FadeOut", lifetime);
 	}
 
 	void FadeOut()
 	{
 		isFading = true;
-		star_anim.SetBool ("isFading", isFading);
-	}
-
-	void Disable()
-	{
-		gameObject.SetActive (false);
 	}
 
 	void OnDisable()
 	{
+		timer = 0.0f;
+		isFading = false;
 		CancelInvoke ();
 	}
 
